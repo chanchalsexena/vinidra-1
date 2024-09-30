94eec320fe994a5bf2207c2ba012fec7ae60e5bc
chanchalsexena <124894666+chanchalsexena@users.noreply.github.com>
2024-10-01 02:50:22 +0530

    Delete client directory

--- client/src/components/Profile/FileUpload.js
+++
@@ -1,9 +0,0 @@
-export const fileUploadCss = {
-    cursor: 'pointer',
-    marginLeft: '-5%',
-    width: '110%',
-    border: 'none',
-    height: '100%',
-    color: '#ECC94B',
-    backgroundColor: 'white',
-};
